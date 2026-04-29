7c1bcb2f1c156dcc9307345a8d33209157ad3880
samuelandradematos <sammattos@aluno.unb.br>
2026-04-29 19:04:06 -0300

    Atualização no algoritimo de passsagem única, esqueleto pronto. TODO: parser

--- include/passagem_unica.hpp
+++ include/passagem_unica.hpp
@@ -1,13 +1,20 @@
 #include <instrucoes.hpp>
+#include <fstream>
 
-unordered_map<string,tuple<bool,list<int>>> tabelaDePendencias;
+unordered_map<string, tuple<bool, int, list<int>>> tabelaDePendencias;
 
 bool IsInstrucao(string);
 bool IsDiretiva(string);
 string GetOpcodeInstrucao(string);
 int GetQuantidadeOperandosInstrucao(string);
 int GetQuantidadeOperandosDiretiva(string);
-void InserePendencia(string, int);
-bool IsDefinido(string);
+void InsertPendencia(string,  int,  bool);
+void UpdateListaPendencias(string label,  int posicaoMemoria);
+bool LabelDefinido(string);
 void PrintTabelaDePendencias();
-
+tuple<string, int> resolveInstrucao(string, string);
+void InsertSimbolo(string, bool, int);
+void TraduzOpcode(string, string);
+int GetEndereco(string);
+bool LabelDefinido(string);
+void Parser(string);
--- src/passagem_unica.cpp
+++ src/passagem_unica.cpp
@@ -1,7 +1,7 @@
 #include <passagem_unica.hpp>
 
 bool IsInstrucao(string instrucao) {
-    unordered_map<string, tuple<int,string>>::const_iterator it = instrucoes.find(instrucao);
+    unordered_map<string,  tuple<int, string>>::const_iterator it = instrucoes.find(instrucao);
     if (it != instrucoes.end())
         return true;
     else 
@@ -17,7 +17,7 @@ bool IsDiretiva(string diretiva) {
 }
 
 string GetOpcodeInstrucao(string instrucao) {
-    unordered_map<string, tuple<int,string>>::const_iterator it = instrucoes.find(instrucao);
+    unordered_map<string,  tuple<int, string>>::const_iterator it = instrucoes.find(instrucao);
 
     string opcode = get<1>(it->second);
 
@@ -25,7 +25,7 @@ string GetOpcodeInstrucao(string instrucao) {
 }
 
 int GetQuantidadeOperandosInstrucao(string instrucao) {
-    unordered_map<string, tuple<int,string>>::const_iterator it = instrucoes.find(instrucao);
+    unordered_map<string,  tuple<int, string>>::const_iterator it = instrucoes.find(instrucao);
 
     int operandos = get<0>(it->second);
 
@@ -33,15 +33,15 @@ int GetQuantidadeOperandosInstrucao(string instrucao) {
 }
 
 int GetQuantidadeOperandosDiretiva(string diretiva) {
-    unordered_map<string, int>::const_iterator it = diretivas.find(diretiva);
+    unordered_map<string,  int>::const_iterator it = diretivas.find(diretiva);
 
     int operandos = it->second;
 
     return operandos;
 }
 
-bool IsDefinido(string label) {
-    unordered_map<string,tuple<bool,list<int>>>::const_iterator it = tabelaDePendencias.find(label);
+bool LabelDefinido(string label) {
+    unordered_map<string, tuple<bool, int, list<int>>>::const_iterator it = tabelaDePendencias.find(label);
     if (it == tabelaDePendencias.end()) {
         return false;
     }
@@ -50,22 +50,63 @@ bool IsDefinido(string label) {
     }
 }
 
-void InserePendencia(string label, int posicaoMemoria, bool definido) {
+void InsertPendencia(string label, int posicaoMemoria, bool definido) {
     list<int> posicaoPendencia;
     posicaoPendencia.emplace_front(posicaoMemoria);
-    tabelaDePendencias.emplace(label, tuple(definido, posicaoPendencia));
-}
-
-void InserePosicaoEmPendencia(string label, int posicaoMemoria) {
-    get<1>(tabelaDePendencias.find(label)->second).emplace_front(posicaoMemoria);
+    tabelaDePendencias.emplace(label, tuple(definido, posicaoMemoria, posicaoPendencia));
 }
 
 void PrintTabelaDePendencias() {
-    cout << "Label  " << "      |       " << "  Definido    " << "      |       " << "Pendencias" << endl;
+    cout << "Label  " << "      |       " << "  Definido    " << "      |       " << "  Endereço Real   " << "      |       " << "  Pendencias" << endl;
+    string auxOutput = "";
     for (auto& it: tabelaDePendencias) {
-        cout << it.first << "           |           " << (get<0>(it.second) ? "T" : "F") << "         |           ";
-        for (auto& k: get<1>(it.second))
-            cout << k;
-        cout << endl;
+        auxOutput += it.first + "           |           " + (get<0>(it.second) ? "T" : "F") + "         |           " + to_string(get<1>(it.second)) + "         |           ";
+        for (auto& k: get<2>(it.second))
+            auxOutput += "[" + to_string(k) + ", ";
+        auxOutput.substr(0,  auxOutput.size() - 2);
+        auxOutput += "]";
+        cout << auxOutput << endl;
     }
+}
+
+void InsertSimbolo(string label, bool definido, int endereco) {
+    list<string> auxListEndereco;
+    auxListEndereco.emplace_front(endereco);
+    tabelaDePendencias.emplace(label,  tuple(definido,  endereco,  auxListEndereco));
+}
+
+int GetEndereco(string label) {
+    unordered_map<string, tuple<bool, int, list<int>>>::const_iterator it = tabelaDePendencias.find(label);
+    return get<1>(it->second);
+}
+
+void UpdateListaPendencias(string label, int endereco) {
+    get<2>(tabelaDePendencias.find(label)->second).emplace_front(endereco);
+}
+
+tuple<string, int> resolveInstrucao(string opcode, string operando, int posicao) {
+    if (LabelDefinido(operando)) {
+        return tuple(opcode, GetEndereco(operando));
+    }
+    else {
+        unordered_map<string, tuple<bool, int, list<int>>>::const_iterator it = tabelaDePendencias.find(operando);
+        if (it != tabelaDePendencias.end()) {
+            UpdateListaPendencias(operando, posicao + 1);
+        }
+
+    }
+    
+}
+
+void Parser(string path_arquivo) {
+    fstream arquivo;
+    arquivo.open(path_arquivo);
+    
+    // TODO:
+    //      lógica de leitura de cada linha(icluir contador de posição)
+    //      para cada linha - verificar se contém definição de label e se tiver inserir na tabelaDePendencias como label definido
+    //                        verificar se o operando já está na tabelaDePendencias:
+    //                                                                              se estiver e já foi definido, retornar endereço
+    //                                                                              se estiver e não foi definido, adicinar posição na lista de pendências
+    //                                                                              se não estiver, adicionar na tabelaDePendencias
 }
