4b5bec320a67694df9f4b8a0a2dcd6af8842e394
Suyannesara <suyannesara17@gmail.com>
2026-05-01 20:15:02 -0300

    arquivo de teste do pre processamento

---
+++ preProcTeste.asm
@@ -0,0 +1,27 @@
+   ; comentário de teste no topo
+    section     text
+TAM    : EQU 10
+INPUT  OLD_DATA
+LOAD OLD_DATA
+L1      :
+L2   :
+L3:     
+    DIV DOIS ;comentário de teste aqui
+STORE  NEW_DATA
+MULT DOIS
+STORE TMP_DATA
+    LOAD         OLD_DATA   
+SUB TMP_DATA
+STORE TMP_DATA
+OUTPUT TMP_DATA
+COPY NEW_DATA, OLD_DATA
+LOAD OLD_DATA
+JMPP L1
+STOP
+SECTION DATA
+DOIS:       
+    CONST TAM
+OLD_DATA: SPACE
+NEW_DATA: SPACE
+TMP_DATA: SPACE
+; comentário de teste no fim
