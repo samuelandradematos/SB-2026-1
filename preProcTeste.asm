ecf29ef8b1078f12b76d6cf85ff23bd87cb1c925
Suyannesara <suyannesara17@gmail.com>
2026-05-05 19:22:13 -0300

    pre proc with tabs

--- preProcTeste.asm
+++ preProcTeste.asm
@@ -1,20 +1,20 @@
-   ; comentário de teste no topo
+    ; comentário de teste no topo
 SECTION DATA
 DOIS:       
-    CONST 2
+        CONST 2
 OLD_DATA: SPACE
 NEW_DATA: SPACE
-TMP_DATA: SPACE
+TMP_DATA        : SPACE
 section     text
-TAM    : EQU 10
+TAM     : EQU 10
 ZERO: EQU 0
 IF TAM
 LOAD OLD_DATA
 IF ZERO
 INPUT  OLD_DATA
 L1      :
-L2   :
-L3:     
+L2  :
+L3      :     
     DIV DOIS ;comentário de teste aqui
 STORE  NEW_DATA
 DEZ: CONST TAM
