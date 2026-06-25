cca8ef2d181cfdde0c001670e0eb51f5106f5942
samuelandradematos <samucamattus@gmail.com>
2026-06-25 10:06:03 -0300

    Esqueleto de DIV e Exponenciação

--- Trabalho-2/multiplicacao.asm
+++ Trabalho-2/multiplicacao.asm
@@ -1,6 +1,6 @@
 %include "main.asm"
 segment .data
-error_msg db "OCORREU OVERFLOW" ; Texto a ser exibido quando ocorre overflow
+error_msg db "OCORREU OVERFLOW", 0 ; Texto a ser exibido quando ocorre overflow
 tam_error_msg equ $-error_msg ; Tamanho da mensagem de erro
 zero equ 0x00000000
 
