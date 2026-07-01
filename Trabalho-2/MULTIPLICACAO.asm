4ab1c5cb7454542ae483bebeae49d05298cc6ca5
samuelandradematos <samucamattus@gmail.com>
2026-06-30 21:18:47 -0300

    Limpa inicial após merge

--- Trabalho-2/MULTIPLICACAO.asm
+++ Trabalho-2/MULTIPLICACAO.asm
@@ -1,15 +1,28 @@
-%include "main.asm"
+global multiplicacao
 segment .data
 error_msg db "OCORREU OVERFLOW", 0 ; Texto a ser exibido quando ocorre overflow
 tam_error_msg equ $-error_msg ; Tamanho da mensagem de erro
-zero equ 0x00000000
 
 segment .text
-    global multiplicacao
 
-multiplicacao:
+multiplicacao_16:
     enter 0,0
-    push eax
+    push edx
+    mov dx, 0x00000000
+    mov ax, [ebp + 12]
+    imul ax, [ebp + 8]    
+    cmp edx, 0x00000000
+    je retorno
+    push tam_error_msg
+    push error_msg
+    call print_string
+    ; Exit Syscall
+	mov eax, 1 ; syscall sair
+	mov ebx, 0
+	int 0x80
+
+multiplicacao_32:
+    enter 0,0
     push edx
     mov edx, zero
     mov eax, [ebp + 12]
@@ -19,7 +32,7 @@ multiplicacao:
     push tam_error_msg
     push error_msg
     call print_string
-; Exit Syscall
+    ; Exit Syscall
 	mov eax, 1 ; syscall sair
 	mov ebx, 0
 	int 0x80
