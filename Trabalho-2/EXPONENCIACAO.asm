10632e1f42f92ae69dd1130e92e48af09f0c5657
samuelandradematos <samucamattus@gmail.com>
2026-07-07 17:52:44 -0300

    Correção de multiplicação e exponenciação

--- Trabalho-2/EXPONENCIACAO.asm
+++ Trabalho-2/EXPONENCIACAO.asm
@@ -12,6 +12,10 @@ exponenciacao_32:
     push ecx
     sub ebx, ebx            ; Zera EBX
     mov ecx, [ebp + 8]      ; Salva em ECX operando 2(expoente)
+    cmp ecx, 0               ; Verifica se o expoente é zero
+    je  .zero_exp_32
+    cmp ecx, 1               ; Verfica se o expoente é 1
+    je .um_exp_32           
     push [ebp + 12]         ; Empilha operando 1(base), como operando 2
     push [ebp + 12]         ; Empilha operando 1(base), como operando 1
     call multiplicacao_32   ; Chama multiplicacao_32 para realizar a operação
@@ -26,6 +30,21 @@ exponenciacao_32:
         call multiplicacao_32   ; Chama multiplicacao_32 para realizar a operação
         sub ecx, 1              
         jnz .loop_exp_32        ; Realiza mais um loop
+        pop ecx
+        pop ebx
+        leave
+        ret
+    
+    .zero_exp_32:
+        mov eax, 1
+        pop ecx
+        pop ebx
+        leave
+        ret
+    .um_exp_32:
+        mov eax, [ebp + 12]
+        pop ecx
+        pop ebx
         leave
         ret
 
@@ -35,6 +54,10 @@ exponenciacao_16:
     push ecx
     sub ebx, ebx            ; Zera EBX, embora vai usar apenas BX
     mov cx, [ebp + 8]       ; Salva em CX operando 2(expoente)
+    cmp cx, 0               ; Verifica se o expoente é zero
+    je  .zero_exp_16
+    cmp cx, 1               ; Verfica se o expoente é 1
+    je .um_exp_16           
     push [ebp + 12]         ; Empilha operando 1(base), como operando 2 da multiplicação
     push [ebp + 12]         ; Empilha operando 2(base), como operando 1 da multiplicação
     call multiplicacao_16   ; Chama multiplicacao_16 para realizar a operação
@@ -49,5 +72,19 @@ exponenciacao_16:
         call multiplicacao_16
         sub cx, 1
         jnz .loop_exp_16
+        leave
+        ret
+
+    .zero_exp_16:
+        mov ax, 1
+        pop ecx
+        pop ebx
+        leave
+        ret
+
+    .um_exp_16:
+        mov ax, [ebp + 12]
+        pop ecx
+        pop ebx
         leave
         ret
--- Trabalho-2/MULTIPLICACAO.asm
+++ Trabalho-2/MULTIPLICACAO.asm
@@ -12,7 +12,8 @@ multiplicacao_16:
     enter 0,0
     push edx
     mov ax, [ebp + 12]
-    imul ax, [ebp + 8]
+    mov bx, [ebp + 8]
+    imul bx
     movsx eax, ax    
     cmp dx, 0x0000
     je retorno
@@ -30,7 +31,7 @@ multiplicacao_32:
     push edx
     mov edx, 0x00000000
     mov eax, [ebp + 12]
-    imul eax, [ebp + 8]
+    imul [ebp + 8]
     cmp edx, 0x00000000
     je retorno
     push dword tam_error_msg
